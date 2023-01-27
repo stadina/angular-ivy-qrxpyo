7cf9745a6db427461aa5cc519675c482ef14f81b
stadina <116000891+stadina@users.noreply.github.com>
2023-01-27 16:28:27 +0100

    5 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
+import {ImageModule} from 'primeng/image';
+
+
+import {DockModule} from 'primeng/dock';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import { CardComponent } from './card.component';
+import {DropdownModule} from 'primeng/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, CardModule],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, CardModule, ImageModule, DockModule, DropdownModule ],
   declarations: [ AppComponent, HelloComponent, CardComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
